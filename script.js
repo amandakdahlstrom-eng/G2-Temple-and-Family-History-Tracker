953d9e896690254a22b75476e12a798b84d8a002
Amanda Dahlstrom <amandak.dahlstrom@gmail.com>
2025-08-15 13:49:00 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Set your goal progress here (0 to 100)
+let progress = 65;
+
+document.getElementById('fill').style.height = progress + '%';
+document.getElementById('goalText').innerText = progress + '%';
