4e7e208dfaad043741aa6180a58afca413303347
Amanda Dahlstrom <amandak.dahlstrom@gmail.com>
2025-09-05 19:21:14 -0600

    Update script.js

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ async function fetchProgress() {
 
 window.onload = function () {
   createPuzzle('puzzle1', 'temple.webp', 5, 5);
-  createPuzzle('puzzle2', 'indexing.jpeg', 5, 5);
+  createPuzzle('puzzle2', 'Indexing.jpeg', 5, 5);
   fetchProgress();
   
 };
